b424db6e5d3324cc2f5dcf185a089f6327add81f
Andrew VanderToorn <andrew.vandertoorn@gmail.com>
2023-01-15 11:02:44 -0500

    Added CreateContactMessageEndpoint.cs and added reference to FastEndpoints

---
+++ API/Endpoints/CreateContactMessageEndpoint.cs
@@ -0,0 +1,14 @@
+﻿using API.Contracts.Requests;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Endpoints;
+
+[HttpPost("contactmessages"), AllowAnonymous]
+public class CreateContactMessageEndpoint : Endpoint<CreateContactMessageRequest>
+{
+    public override Task HandleAsync(CreateContactMessageRequest req, CancellationToken ct)
+    {
+        return base.HandleAsync(req, ct);
+    }
+}
