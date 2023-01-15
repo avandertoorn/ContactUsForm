9ad8ef3345b557073c4b9092e270577b0a7e8cbf
Andrew VanderToorn <andrew.vandertoorn@gmail.com>
2023-01-15 09:53:49 -0500

    Added request and response for contact message

---
+++ API/Contracts/Requests/CreateContactMessageRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Contracts.Requests;
+
+public sealed class CreateContactMessageRequest
+{
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public string Email { get; init; }
+    public string Message { get; init; }
+}
---
+++ API/Contracts/Responses/ValidationFailureResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace API.Contracts.Responses;
+
+public sealed class ValidationFailureResponse
+{
+    public List<string> Errors { get; init; } = new();
+}
