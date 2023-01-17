862bf34629e54490cc1c7269f08811e94a6d4288
Andrew VanderToorn <andrew.vandertoorn@gmail.com>
2023-01-16 21:17:40 -0500

    Added validator for CreateContactMessageRequest

---
+++ API/Validation/CreateContactMessageValidator.cs
@@ -0,0 +1,15 @@
+﻿using API.Contracts.Requests;
+using FluentValidation;
+
+namespace API.Validation;
+
+public class CreateContactMessageValidator : AbstractValidator<CreateContactMessageRequest>
+{
+    public CreateContactMessageValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty();
+        RuleFor(x => x.LastName).NotEmpty();
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Message).NotEmpty();
+    }
+}
