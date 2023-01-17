50d48aa355b73814a3056a008dd97b749bce1428
Andrew VanderToorn <andrew.vandertoorn@gmail.com>
2023-01-16 21:19:28 -0500

    Added validation middleware

---
+++ API/Validation/ValidationExceptionMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using API.Contracts.Responses;
+using FluentValidation;
+
+namespace API.Validation;
+
+public class ValidationExceptionMiddleware
+{
+    private readonly RequestDelegate _request;
+
+    public ValidationExceptionMiddleware(RequestDelegate request)
+    {
+        _request = request;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _request(context);
+        }
+        catch (ValidationException ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            var message = ex.Errors.Select(x => x.ErrorMessage).ToList();
+            var validationError = new ValidationFailureResponse()
+            {
+                Errors = message
+            };
+            await context.Response.WriteAsJsonAsync(validationError);
+        }
+    }
+}
