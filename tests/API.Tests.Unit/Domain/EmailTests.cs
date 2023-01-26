ebc38798fb035e17eae797ee8c1cb35b5015fdc0
Andrew VanderToorn <andrew.vandertoorn@gmail.com>
2023-01-25 20:59:20 -0500

    Added Email Tests

---
+++ tests/API.Tests.Unit/Domain/EmailTests.cs
@@ -0,0 +1,29 @@
+﻿using API.Domain.Common;
+using FluentAssertions;
+using FluentValidation;
+
+namespace API.Tests.Unit.Domain;
+
+public class EmailTests
+{
+    [Fact]
+    public async Task EmailFrom_ShouldSucceed_WhenEmailIsValid()
+    {
+        var email = "johndoe@email.com";
+
+        var emailType = Email.From(email);
+
+        emailType.Should().NotBeNull();
+        emailType.Value.Should().Be(email);
+    }
+
+    [Fact]
+    public async Task EmailFrom_ShouldThrowValidationException_WhenEmailIsInvalid()
+    {
+        var email = "invalidEmail.com";
+
+        Action result = () => Email.From(email);
+        
+        result.Should().Throw<ValidationException>();
+    }
+}
