9ff7728dfc631c0ee59783324dbcb85b2f8ae150
Andrew VanderToorn <andrew.vandertoorn@gmail.com>
2023-01-25 20:59:10 -0500

    Added ContactMessageIdTests

---
+++ tests/API.Tests.Unit/Domain/ContactMessageIdTests.cs
@@ -0,0 +1,30 @@
+﻿using API.Domain.Common;
+using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace API.Tests.Unit.Domain;
+
+public class ContactMessageIdTests
+{
+    [Fact]
+    public async Task From_ShouldSucceed_WhenValid()
+    {
+        var contactMessageId = Guid.NewGuid();
+        
+        var contactMessageResult = ContactMessageId.From(contactMessageId);
+
+        contactMessageResult.Should().NotBeNull();
+        contactMessageResult.Value.Should().Be(contactMessageId);
+    }
+    
+    [Fact]
+    public async Task From_ShouldThrowValidationException_WhenEmpty()
+    {
+        var contactMessageId = Guid.Empty;
+        
+        Action result = () => ContactMessageId.From(contactMessageId);
+
+        result.Should().Throw<ArgumentException>();
+    }
+}
