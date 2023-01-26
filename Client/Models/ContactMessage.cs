5de633113a69551b950df2eda8d8fb516e9ec2ad
Andrew VanderToorn <andrew.vandertoorn@gmail.com>
2023-01-25 22:05:03 -0500

    Added defualts to ContactMessage

--- Client/Models/ContactMessage.cs
+++ Client/Models/ContactMessage.cs
@@ -8,16 +8,16 @@ public class ContactMessage
     [Required]
     [MinLength(1)]
     [MaxLength(50)]
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } = String.Empty;
+
     [Required]
     [MinLength(1)]
     [MaxLength(50)]
-    public string LastName { get; set; }
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; }
+    public string LastName { get; set; } = String.Empty;
+
+    [Required] [EmailAddress] public string Email { get; set; } = String.Empty;
     [Required]
     [MinLength(20, ErrorMessage = "Message cannot be shorter than 20 characters")]
     [MaxLength(500, ErrorMessage = "Message cannot be longer than 500 characters")]
-    public string Message { get; set; }
+    public string Message { get; set; } = String.Empty;
 }
